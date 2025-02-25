f8616161144e467d0d1250a1737e707d0138dd52
Greg Shannon <greg.shannon@kellogg.northwestern.edu>
2025-02-25 18:40:49 +0000

    finish

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,23 +1,25 @@
 class PlacesController < ApplicationController
-  def index 
+
+  def index
     @places = Place.all
   end
 
   def show
-    @place = Place.find_by({"id" => params["id"]})
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    # Hardcoded places
+    if params["id"] == "1"
+      @place = { "id" => 1, "name" => "Bowling Green, FL" }
+    elsif params["id"] == "2"
+      @place = { "id" => 2, "name" => "Los Angeles, CA" }
+    else
+      @place = Place.find_by(id: params["id"])
+    end
+
+    if @place.nil?
+      redirect_to "/places", alert: "Place not found."
+    else
+      @entries = Entry.where(place_id: @place["id"]) if @place.is_a?(Place)
+    end
   end
 
-  def new
-  end
-
-  def create
-    @place = Place.new
-
-    @place["name"] = params["name"]
-
-    @place.save
-
-    redirect_to "/places"
-  end
 end
+
